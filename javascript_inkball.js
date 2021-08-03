c2cc46ed97f675290cc6e40c40b7c873ea765fe2
Shubham <71990977+Skparab1@users.noreply.github.com>
2021-08-02 18:03:35 -0700

    Add files via upload

--- javascript_inkball.js
+++ javascript_inkball.js
@@ -6,27 +6,33 @@ function setup() {
 var ball_x = 1000;
 var ball_y = 500;
 
-var ball_speed = 7;
+var ball_speed_x = 7;
+
+var ball_speed_y = 7
 
 var ball_direction = 'right';
 
+var drawnpoints = [];
+
+var drawing = false;
+
 function animate_ball(){
   if (ball_direction == 'right'){
-    ball_x = ball_x + ball_speed;
+    ball_x = ball_x + ball_speed_x;
   } else if (ball_direction == 'rightup'){
-    ball_x = ball_x + ball_speed;
-    ball_y = ball_y - 7;
+    ball_x = ball_x + ball_speed_x;
+    ball_y = ball_y - ball_speed_y;
   } else if (ball_direction == 'rightdown'){
-    ball_x = ball_x + ball_speed;
-    ball_y = ball_y + 7;
+    ball_x = ball_x + ball_speed_x;
+    ball_y = ball_y + ball_speed_y;
   } else if (ball_direction == 'left'){
-    ball_x = ball_x - ball_speed;
+    ball_x = ball_x - ball_speed_x;
   } else if (ball_direction == 'leftup'){
-    ball_x = ball_x - ball_speed;
-    ball_y = ball_y - 7;
+    ball_x = ball_x - ball_speed_x;
+    ball_y = ball_y - ball_speed_y;
   } else if (ball_direction == 'leftdown'){
-    ball_x = ball_x - ball_speed;
-    ball_y = ball_y + 7;
+    ball_x = ball_x - ball_speed_x;
+    ball_y = ball_y + ball_speed_y;
   }
   
   if (ball_y >= 793 && ball_direction == 'leftdown'){
@@ -48,7 +54,7 @@ function animate_ball(){
     } else {
        ball_direction = 'rightup';
     }
-    ball_speed = Math.floor(Math.random() * 12) + 7;
+    ball_speed_x = Math.floor(Math.random() * 12) + 7;
   }  
   
   if (ball_x >= 1793){
@@ -61,7 +67,7 @@ function animate_ball(){
            ball_direction = 'leftup';
         }
         
-    ball_speed = Math.floor(Math.random() * 12) + 7;
+    ball_speed_x = Math.floor(Math.random() * 12) + 7;
      
   }
 }
@@ -85,4 +91,38 @@ function draw() {
   ellipse(ball_x,ball_y,50,50);
   
   setTimeout(animate_ball, 1000);
+  
+  fill(0,0,0);
+  stroke(0,0,0);
+  
+  if (drawing){
+    try{
+      for(i = 0;i<=5000;i+=2){
+        ellipse(drawnpoints[i],drawnpoints[i+1],30,30);
+        
+        let directdistance = Math.sqrt(((ball_x - drawnpoints[i])*(ball_x - drawnpoints[i]))+((ball_y - drawnpoints[i+1])*(ball_y - drawnpoints[i+1])));
+        
+        if (directdistance <= 60){
+          ball_speed_x = -1 * ball_speed_x;
+          ball_speed_y = -1 * ball_speed_y;
+        }
+    
+    }
+
+    } catch(error){
+      let blank = '';
+    }
+    
+  }
+  }
+
+function mouseDragged(){
+  
+  drawing = true;
+  
+  ellipse(mouseX,mouseY,100,100);
+  
+  drawnpoints.push(mouseX);
+  drawnpoints.push(mouseY);
+  
 }
